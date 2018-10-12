5c2367b9a6d0ebc575511287b8eefb0a116af88b
era ruddra <eraruddra@gmail.com>
2018-10-12 12:26:42 +0530

    tumhara dabba logo

--- newwebsite.js
+++ newwebsite.js
@@ -1,20 +1,4 @@
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function sliding(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function sliding(n) {
-  showSlides(slideIndex = n);
-}
-
-  function showSlides(n) {
-     var i;
-     var slides = document.getElementsByClassName("mySlides");
-   }
 
   
 opencart = ()=> {
